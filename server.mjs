23294c5f232ef4181ca2ea2b4bbb02566fc9e64a
Jessica Y <desh365@hotmail.com>
2018-11-17 01:01:51 -0600

    Use environment port

--- server.mjs
+++ server.mjs
@@ -1,24 +1,24 @@
-import express from 'express'
-import exphbs from 'express-handlebars'
-import { router } from './controllers/burgers_controller'
-import bodyParser from 'body-parser'
+import express from "express"
+import exphbs from "express-handlebars"
+import { router } from "./controllers/burgers_controller"
+import bodyParser from "body-parser"
 
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8090
 
 //orm.insertBurger('BigBurger').then((res) => console.log(res))
 
 //orm.selectAllBurgers().then(res => console.log(res))
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs({ defaultLayout: "main" }))
+app.set("view engine", "handlebars")
 //use body parser to parse forms
 //note this must be above the use of the router
 app.use(bodyParser.urlencoded({ extended: true }))
 //use the routes we created
 app.use(router)
 // serve static content like our css file
-app.use(express.static('public'))
+app.use(express.static("public"))
 
 app.listen(PORT, () =>
   console.log(`Burger server is hungrily awaiting requests on port ${PORT}`)
